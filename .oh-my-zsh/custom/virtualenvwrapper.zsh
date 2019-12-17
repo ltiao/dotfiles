5bce01d0171c165a3c56ccd3ef902f3ee9aa1189
Louis Tiao <louistiao@gmail.com>
2019-12-17 22:21:57 +0000

    Added zsh startup files

---
+++ .oh-my-zsh/custom/virtualenvwrapper.zsh
@@ -0,0 +1,5 @@
+# Virtualenvwrapper
+export VIRTUALENVWRAPPER_PYTHON=$(which python3.8)
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Projects
+source $(which virtualenvwrapper.sh)
